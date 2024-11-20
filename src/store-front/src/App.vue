6e718e0c0be4122f1df7094b48d3faf28e230df2
Márcio Viveiros <marcioviveiros123@hotmail.com>
2024-11-20 23:09:40 +0000

    Adding things to test the agrodcd-updater

--- src/store-front/src/App.vue
+++ src/store-front/src/App.vue
@@ -92,15 +92,15 @@ export default {
         .then(response => {
           console.log(response)
           if (!response.ok) {
-            alert('Error occurred while submitting order! Now i know the code is changed and the kubernets change automatically :) Apresentação Sistemas Distribuidos!!')
+            alert('Error occurred while submitting order! Now i know the code is changed and the kubernets change automatically :) Apresentação Sistemas Distribuidos!!!')
           } else {
             this.cartItems = []
-            alert('Order submitted successfully! Now i know the code is changed and the kubernets change automatically :) Apresentação Sistemas Distribuidos!')
+            alert('Order submitted successfully! Now i know the code is changed and the kubernets change automatically :) Apresentação Sistemas Distribuidos!!!')
           }
         })
         .catch(error => {
           console.log(error)
-          alert('Error occurred while submitting order! Now i know the code is changed and the kubernets change automatically :) Apresentação Sistemas Distribuidos!')
+          alert('Error occurred while submitting order! Now i know the code is changed and the kubernets change automatically :) Apresentação Sistemas Distribuidos!!!')
         })
     }
   },
